8bc302992021dce3ced9a281d176044c0d83e838
sahnavin123 <sahnavin123@gmail.com>
2023-01-30 15:30:42 +0530

    github-auth

---
+++ services/functions/private.ts
@@ -0,0 +1,6 @@
+export async function handler(){
+    return {
+        statusCode: 200,
+        body: "hello this is private page"
+    }
+}
---
+++ services/functions/public.ts
@@ -0,0 +1,6 @@
+export async function handler(){
+    return {
+        statuscode: 200,
+        body: "hello this is public page"
+    }
+}
